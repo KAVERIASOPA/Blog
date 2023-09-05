c55e8fc90873cf86046e5de5f28b6484c7a19676
Kaveri Asopa <91665513+KAVERIASOPA@users.noreply.github.com>
2023-09-05 10:06:30 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <section className='w-full flex-center flex-col'>
             <h1 className='head_text text-center'>
-                Discover & Share
+                Your Thoughts, Your Voice, Your Blog.
                 <br className='max-md:hidden' />
                 <span className='orange_gradient text-center'>
                    Create Your Blogs
